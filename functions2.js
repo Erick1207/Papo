c30cfba59240effef0e949f3a7b3ded21f16b377
huguinh0 <fonseca.hugo@escolar.ifrn.edu.br>
2026-05-10 21:48:15 -0300

    novas funcionalidades

--- functions2.js
+++ functions2.js
@@ -1,23 +1,66 @@
 document.addEventListener('DOMContentLoaded', () => {
+
     const grupos = document.querySelectorAll('.grupo1, .grupo2, .grupo3, .grupo4, .grupo5');
     const conversas = document.querySelectorAll('.conversa-content');
-
+    const inputMensagem = document.getElementById('mensagem');
+    const botaoEnviar = document.getElementById('enviar');
+    let conversaAtual = null;
     function esconderTodasConversas() {
         conversas.forEach(conversa => {
             conversa.classList.remove('active');
+
         });
+
     }
 
     document.getElementById('conversa-padrao').classList.add('active');
+   grupos.forEach(grupo => {
 
-    grupos.forEach(grupo => {
-        grupo.addEventListener('click', () => {
-            const grupoId = grupo.id.replace('grupo-', 'conversa-');
-            esconderTodasConversas();
-            const conversa = document.getElementById(grupoId);
-            if (conversa) {
-                conversa.classList.add('active');
-            }
-        });
+    grupo.addEventListener('click', () => {
+
+        esconderTodasConversas();
+        const grupoId = grupo.id.replace('grupo-', 'conversa-');
+        conversaAtual = document.getElementById(grupoId);
+
+        if (conversaAtual) {
+            conversaAtual.classList.add('active');
+            inputMensagem.value = '';
+
+        }
+
     });
+
+});
+
+    function resetarMensagens(conversa) {
+        const mensagensDinamicas = conversa.querySelectorAll('.mensagem.usuario');
+        mensagensDinamicas.forEach(msg => {
+            msg.remove();
+
+        });
+
+    }
+    botaoEnviar.addEventListener('click', enviarMensagem);
+    inputMensagem.addEventListener('keypress', function(event){
+        if(event.key === 'Enter'){
+            enviarMensagem();
+
+        }
+
+    });
+
+    function enviarMensagem() {
+        if (!conversaAtual) return;
+        const texto = inputMensagem.value.trim();
+        if (texto === '') return;
+        const novaMensagem = document.createElement('div');
+        novaMensagem.classList.add('mensagem');
+        novaMensagem.classList.add('usuario');
+        novaMensagem.innerHTML = `<p>${texto}</p>`;
+        conversaAtual.appendChild(novaMensagem);
+        inputMensagem.value = '';
+        conversaAtual.scrollTop = conversaAtual.scrollHeight;
+
+    }
+
 });
